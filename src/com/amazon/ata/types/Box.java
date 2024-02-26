c3222b29680d655a7f511967eb550d3d3539161b
Julie Fiess <juliefiess@gmail.com>
2024-02-26 07:41:56 -0700

    First submission

---
+++ src/com/amazon/ata/types/Box.java
@@ -0,0 +1,98 @@
+package com.amazon.ata.types;
+
+import java.math.BigDecimal;
+
+public class Box extends Packaging {
+    /**
+     * The material this packaging is made of.
+     */
+    private Material CORRUGATE;
+
+    /**
+     * This packaging's length.
+     */
+    private BigDecimal length;
+
+    /**
+     * This packaging's smallest dimension.
+     */
+    private BigDecimal width;
+
+    /**
+     * This packaging's largest dimension.
+     */
+    private BigDecimal height;
+
+    /**
+     * Instantiates a new Packaging object.
+     *
+     * @param material - the Material of the package
+     * @param length   - the length of the package
+     * @param width    - the width of the package
+     * @param height   - the height of the package
+     */
+    public Box(Material material, BigDecimal length, BigDecimal width, BigDecimal height) {
+        super(material);
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public BigDecimal getLength() {
+        return length;
+    }
+
+    public BigDecimal getWidth() {
+        return width;
+    }
+
+    public BigDecimal getHeight() {
+        return height;
+    }
+
+    /**
+     * Returns whether the given item will fit in this packaging.
+     *
+     * @param item the item to test fit for
+     * @return whether the item will fit in this packaging
+     */
+    //@Override
+    public boolean canFitItem(Item item) {
+        return this.length.compareTo(item.getLength()) > 0 &&
+                this.width.compareTo(item.getWidth()) > 0 &&
+                this.height.compareTo(item.getHeight()) > 0;
+    }
+
+    /**
+     * Returns the mass of the packaging in grams. The packaging weighs 1 gram per square centimeter.
+     *
+     * @return
+     */
+    //@Override
+    public BigDecimal getMass() {
+        BigDecimal two = BigDecimal.valueOf(2);
+
+        // For simplicity, we ignore overlapping flaps
+        BigDecimal endsArea = length.multiply(width).multiply(two);
+        BigDecimal shortSidesArea = length.multiply(height).multiply(two);
+        BigDecimal longSidesArea = width.multiply(height).multiply(two);
+
+        return endsArea.add(shortSidesArea).add(longSidesArea);
+    }
+
+    /*
+    @Override
+    public boolean canFitItem(Item item) {
+        return super.canFitItem(item);
+    }
+
+    @Override
+    public BigDecimal getMass() {
+        endsArea = length * width * 2;
+        shortSidesArea = length * height * 2;
+        longSidesArea = width * height * 2;
+        mass = endsArea + shortSidesArea + longSidesArea;
+        return super.getMass();
+    }
+    */
+}
---
+++ src/com/amazon/ata/types/PolyBag.java
@@ -0,0 +1,35 @@
+package com.amazon.ata.types;
+
+import java.math.BigDecimal;
+
+public class PolyBag extends Packaging {
+    private BigDecimal volume;
+    /**
+     * The material this packaging is made of.
+     */
+    private Material LAMINATED_PLASTIC;
+    /**
+     * Instantiates a new Packaging object.
+     *
+     * @param material - the Material of the package
+     */
+    public PolyBag(Material material, BigDecimal volume) {//, BigDecimal length, BigDecimal width, BigDecimal height) {
+        super(material);
+        this.volume = volume;
+    }
+
+    @Override
+    public boolean canFitItem(Item item) {
+        return super.canFitItem(item);
+    }
+
+    @Override
+    public BigDecimal getMass() {
+        double mass = Math.ceil(Math.sqrt(volume.doubleValue()) * 0.6);
+        return BigDecimal.valueOf(mass);
+    }
+
+    public BigDecimal getVolume() {
+        return volume;
+    }
+}
